87d183300714b8110aac93641777bb28b83dfd13
MRmlik12 <daniel.olczyk.contact@proton.me>
2023-06-21 18:02:32 +0200

    Add some styling for search box

--- src/app/components/appbar/SearchBox.tsx
+++ src/app/components/appbar/SearchBox.tsx
@@ -1,10 +1,12 @@
 import styles from "./SearchBox.module.sass";
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { debounce } from "lodash";
 import { useSetRecoilState } from "recoil";
 import { searchBoxInput } from "@/app/states";
+import { IoSearch } from "react-icons/io5";
 
 const SearchBox = () => {
+  const [showInput, setShowInput] = useState(false);
   const setInput = useSetRecoilState(searchBoxInput);
 
   const debounceSearch = useRef(
@@ -16,8 +18,13 @@ const SearchBox = () => {
   };
 
   return (
-    <div className={styles.searchBox}>
-      <input type="text" onInputCapture={handleInputChanges} />
+    <div className={styles.searchBoxContainer}>
+      <input
+        className={styles.searchBox}
+        type="text"
+        onInputCapture={handleInputChanges}
+      />
+      <IoSearch className={styles.icon} />
     </div>
   );
 };
